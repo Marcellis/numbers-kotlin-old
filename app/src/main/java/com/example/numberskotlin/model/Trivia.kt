c59503dead814d292afc96ac6f579705521fee36
Kevin Martin <kevin.martin@hva.nl>
2019-06-12 20:12:08 +0200

    Remove Room dependency and Add SerializedName in Trivia data class

--- app/src/main/java/com/example/numberskotlin/model/Trivia.kt
+++ app/src/main/java/com/example/numberskotlin/model/Trivia.kt
@@ -1,13 +1,14 @@
 package com.example.numberskotlin.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Trivia(
-    var text: String,
-    var number: Int,
-    var found: Boolean,
-    var type: String
+    @SerializedName("text") var text: String,
+    @SerializedName("number") var number: Int,
+    @SerializedName("found") var found: Boolean,
+    @SerializedName("type") var type: String
 ) : Parcelable
 
